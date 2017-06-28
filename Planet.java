c1afc3ddb8a63bb405005a79177d23121b806175
Jon Miguel  Jara <jonmigueljara@Jons-MacBook-Pro.local>
2017-06-27 22:37:18 -0700

    push all files.

---
+++ Planet.java
@@ -0,0 +1,11 @@
+public class Planet {
+	double xxPos;
+	double yyPos;
+	double xxVel;
+	double yyVel;
+	double mass;
+	String imgFilename;
+
+	public Planet(double xP, double yP, double xV,
+              double yV, double m, String img);
+}
